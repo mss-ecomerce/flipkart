bfde256828377896ecb4b9c6b06977f92d1ed6ee
mvenkatrao <venkatrao4u111@gmail.com>
2019-10-13 23:44:57 +0530

    Git Chery-pick

--- Connected.java
+++ Connected.java
@@ -2,4 +2,5 @@
 For Checking Purpose We are adding the somme data in This is For Branching stategy
 
 For Checking Purpose We are adding the somme data in This is the master branch checking
- and now git stash 
+ and now git stash it's For Git Chery-pick
+
