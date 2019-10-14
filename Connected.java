75ad12891002e2c30e95de2431052a7f764540ac
mvenkatrao <venkatrao4u111@gmail.com>
2019-10-14 23:50:59 +0530

    updated code

--- Connected.java
+++ Connected.java
@@ -5,4 +5,6 @@ For Checking Purpose We are adding the somme data in This is the master branch c
  and now git stash it's For Git Chery-pick
 switching to another branch For SSH Keye some changes done For ssh
 successfully done sshkeyggen
-changing in remote repo
+changing in remote repo 
+tring to push thuis code to my remote repo:wq
+
